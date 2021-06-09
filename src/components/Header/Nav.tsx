632a218267f7b28edb5aac9bfd4a68ce41d6f05f
SafeMoon Cash <team@safemooncash.org>
2021-06-09 23:31:28 +0100

    SmCSwap 1.0.1

---
+++ src/components/Header/Nav.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import styled from 'styled-components'
+import TranslatedText from '../TranslatedText'
+
+const Nav: React.FC = () => {
+  return (
+    <StyledNav>
+      <StyledAbsoluteLink href="https://safemooncash.org/">
+        <TranslatedText translationId={2}>Back To Base</TranslatedText>
+      </StyledAbsoluteLink>
+    </StyledNav>
+  )
+}
+
+const StyledNav = styled.nav`
+  align-items: center;
+  display: flex;
+  font-size: 20px;
+  line-height: 45px;
+  font-weight: 500;
+  @media (max-width: 600px) {
+    display: none;
+  }
+`
+
+const StyledAbsoluteLink = styled.a`
+  color: #12aab5;
+  padding-left: 10px;
+  padding-right: 10px;
+  text-decoration: none;
+  &:hover {
+    color: #452a7a;
+  }
+  &.active {
+    color: #452a7a;
+  }
+  @media (max-width: 400px) {
+    padding-left: 10px;
+    padding-right: 10px;
+  }
+`
+
+export default Nav
