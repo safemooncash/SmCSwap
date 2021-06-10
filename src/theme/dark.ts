659ad9616e2f132d77b62127141108b7e389e448
safemooncash <team@safemooncash.org>
2021-06-10 20:31:55 +0100

    Update dark.ts

--- src/theme/dark.ts
+++ src/theme/dark.ts
@@ -15,7 +15,7 @@ export default {
     bg1: '#00c2cb',
     bg2: '#00c2cb',
     bg3: '#17113a',
-    bg4: '#00c2cb',
+    bg4: '#FFFFFF',
     bg5: '#17113a',
 
     //specialty colors
@@ -23,15 +23,15 @@ export default {
     advancedBG: 'rgba(0,0,0,0.1)',
 
     //primary colors
-    primary1: '#24c7d6',
+    primary1: '#17113a',
     primary4: '#376bad70',
     primary5: '#153d6f70',
 
     // color text
-    primaryText1: '#000000',
-    buttonText: '#000000',
+    primaryText1: '#6da8ff',
+    buttonText: '#ffffff',
 
     // secondary colors
-    secondary1: '#000000'
+    secondary1: '#24c7d6'
   }
 }
